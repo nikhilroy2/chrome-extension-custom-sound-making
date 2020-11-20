7ed10b9ca00dda81609d7d1b0357841bb5b25e34
Nikhil Roy <fenr1user@biadsconsulting.org>
2020-11-20 20:23:29 +0600

    custom sound added

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 
-
 function FindingSound() {
     let soundScript = document.querySelector('script[src*="https://assetsv2.fiverrcdn.com/assets/dist/entries/fNotificationSound-684357146b95835d4170c5f1f183819b.js?v=1"]');
     soundScript.id = "soundScript"
     soundScript.setAttribute('src', SoundSource())
-    let js_side_nav_overlay = document.querySelector('.js-side-nav-overlay').nextElementSibling;
-    js_side_nav_overlay.innerHTML = '//'
+    let js_side_nav_overlay = document.querySelector('.js-side-nav-overlay');
+
+    js_side_nav_overlay.nextElementSibling.innerHTML = '//'
+    js_side_nav_overlay.previousElementSibling.innerHTML = '//'
 
 
+   
 
     console.log(soundScript)
 }
 window.onload = (() => {
+    
     FindingSound()
 })
 
