8059dfb84d789c05eab2b25e2aa0f16d1111b642
Nikhil Roy <fenr1user@biadsconsulting.org>
2020-11-21 07:58:44 +0600

    button clickable sound ready

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-
-function FindingSound() {
-    let soundScript = document.querySelector('script[src*="https://assetsv2.fiverrcdn.com/assets/dist/entries/fNotificationSound-684357146b95835d4170c5f1f183819b.js?v=1"]');
-    soundScript.id = "soundScript"
-    soundScript.setAttribute('src', SoundSource())
-    let js_side_nav_overlay = document.querySelector('.js-side-nav-overlay');
-
-    js_side_nav_overlay.nextElementSibling.innerHTML = '//'
-    js_side_nav_overlay.previousElementSibling.innerHTML = '//'
-
-
-   
-
-    console.log(soundScript)
-}
 window.onload = (() => {
     
-    FindingSound()
 })
-
+setTimeout(()=> {
+    FindingSound()
+    console.log("Sound Ready")
+},5000)
 
 function SoundSource() {
     // https://www.pacdv.com/sounds/voices/oh-yeah-everything-is-fine.wav
     let getSource = 'https://cdn.jsdelivr.net/gh/nikhilroy2/chrome-extension-custom-sound-making@master/soundScript.js'
     return getSource;
+}
+
+
+function FindingSound() {
+    let soundScript = document.querySelector('script[src*="https://assetsv2.fiverrcdn.com/assets/dist/entries/fNotificationSound-684357146b95835d4170c5f1f183819b.js?v=1"]');
+    //soundScript.id = "soundScript"
+    soundScript.setAttribute('src', SoundSource())
+    let js_side_nav_overlay = document.querySelector('.js-side-nav-overlay');
+
+    js_side_nav_overlay.nextElementSibling.innerHTML = '//'
+    js_side_nav_overlay.previousElementSibling.innerHTML = '//'
+    let msg = new Audio(chrome.runtime.getURL("fiver_custom_sound.mp3"))
+    $('.btn-toggle-sound').click(function() {
+        //msg.play();
+        $(this).hasClass('on') ? msg.pause() : msg.play()
+        
+        
+    })
+
+
+   
+
+    console.log(soundScript)
 }
