693d21f9c981bf1d665fe1a3e49c67175251f3e7
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2021-11-06 18:50:41 +0600

    updated 06 nov 2021

--- soundCdn.js
+++ soundCdn.js
@@ -1,13 +1,12 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        if( details.url == "https://assetsv2.fiverrcdn.com/assets/dist/entries/fNotificationSound-684357146b95835d4170c5f1f183819b.js?v=1" )
-            return {redirectUrl: "http://www.mysite.com/js/library_dev.js" };
+        if( details.url == "https://assetsv2.fiverrcdn.com/assets/dist/entries/fNotificationSound-fd2bfa44e52b8c11ff39a2621c97ce54.js?v=1" )
+            return {redirectUrl: "https://cdn.jsdelivr.net/gh/nikhilroy2/chrome-extension-custom-sound-making@master/nikhil_sound_cdn.js" };
     },
     {urls: ["*://assetsv2.fiverrcdn.com/assets/*.js*"]},
     ["blocking"]);
 
 
-console.log("bg script")    
 
 
 // ://127.0.0.1:5500/
